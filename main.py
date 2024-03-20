69a2ea55071a7be57c6c70eb4ac2336abcb50bf4
Rachel-Greene <159064114+Rachel-Greene@users.noreply.github.com>
2024-03-20 20:03:47 +0000

    Working loop A, B, C

--- main.py
+++ main.py
@@ -1,3 +1,39 @@
 ## This code runs the python code of pesudocode assignment 5-1 ##
 
+## loop A ##
+a = 1
+b = 2
+c = 5
+while a < c:
+  a = a + 1
+  b = b + c
+print(f"Loop A: {a, b, c}")
+
+## loop B ##
+d = 4
+e = 6
+f = 7
+while d > f:
+  d = d + 1
+  e = e-1
+print(f"Loop B: {d, e, f}")
+
+## loop C ##
+g = 4
+h = 6
+while g < h:
+  g = g + 1
+print(f"Loop C: {g, h}")
+
+## loop D ##
+
+
+
+## loop E ##
+
+
+
+## loop F ##
+
+
 
