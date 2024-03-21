2e8bf72f92fef13b91917dcefa8b662689ebcee9
Rachel-Greene <159064114+Rachel-Greene@users.noreply.github.com>
2024-03-21 13:16:14 +0000

    Complete Loop F

--- main.py
+++ main.py
@@ -55,6 +55,15 @@ while j < k:
 
 
 ## loop F ##
-
+print("--- Loop F ---")
+p = 2
+q = 4
+while p < q:
+  print("Adios")
+  r = 1
+  while r < q:
+    print("Adios")
+    r = r + 1
+  p = p + 1
 
 
